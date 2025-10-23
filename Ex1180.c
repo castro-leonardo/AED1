d59e1f5f7e690797e4be1e1940cda1726f0a7cc5
castro-leonardo <oleonardo.castro04@gmail.com>
2025-10-23 16:45:56 -0300

    Ex1180

---
+++ Ex1180.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main () {
+    int N, menor=1000;
+    
+    do{
+        scanf("%i", &N);
+    }while(N<1 || N>1000);
+
+    int X[N];
+
+    for(int i=0; i<N; i++){
+        scanf("%i", &X[i+1]);
+        if(X[i+1] < menor){
+            menor=X[i+1];
+        }
+    }
+
+    printf("%i", menor);
+
+    return 0;
+}
